e38a1515ec06e6cb6f368f1f5d52e4acc0a9fd44
wiki-py <muhumuzawycliffcc@gmaul.com>
2026-05-04 13:48:59 +0300

    Update URL configuration to use surveysystem instead of kingdom_survey
    - Change welcome_view import to use surveysystem.surveys.views
    - Update API URL include to use surveysystem.surveys.urls
    - Remove kingdom_survey references from URL configuration
    - Ensure all imports point to surveysystem module structure

--- kingdom_survey/urls.py
+++ kingdom_survey/urls.py
@@ -19,12 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from surveysystem.surveysystem.surveys.views import welcome_view
+from surveysystem.surveys.views import welcome_view
 
 urlpatterns = [
     path('', welcome_view, name='welcome'),
     path('admin/', admin.site.urls),
-    path('api/', include('survey.urls')),
+    path('api/', include('surveysystem.surveys.urls')),
     
     # API Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
